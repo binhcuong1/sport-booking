e984028d85a310f14e116f99f9ef98aca31c6b35
Bình Cương <135428122+binhcuong1@users.noreply.github.com>
2026-01-04 09:29:52 +0700

    Update BookingRepository.java

--- src/main/java/com/theliems/sport_booking/repository/BookingRepository.java
+++ src/main/java/com/theliems/sport_booking/repository/BookingRepository.java
@@ -2,7 +2,17 @@ package com.theliems.sport_booking.repository;
 
 import com.theliems.sport_booking.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookingRepository
         extends JpaRepository<Booking, Integer> {
+                    @Query("""
+  SELECT (COUNT(b) > 0)
+  FROM Booking b
+  WHERE b.profile.profile_id = :profileId
+    AND b.club.id = :clubId
+    AND b.booking_status = 'hoàn thành'
+  """)
+    boolean hasCompletedBooking(Integer profileId, Integer clubId);
 }
